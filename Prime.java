81a72e23501d04afe8faf6a26a3be05f7111afcc
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-15 22:36:09 +0530

    Prime number

---
+++ Prime.java
@@ -0,0 +1,31 @@
+public class Prime
+{
+    public static void main (String[]args)
+    {
+        int n = 23;
+        checkPrime (n);
+    }
+
+    private static void checkPrime (int n)
+    {
+        int count = 0;
+
+        //negative numbers, 0 and 1 are not prime
+        if (n<2)
+        {
+            System.out.println("The given is number "+ n + " is not prime ");
+            System.exit (0);
+        }
+        for (int i = 1; i <=n; i++)
+        {
+            if (n% i == 0)
+                count += 1;
+        }
+
+        if (count > 2)
+            System.out.println("The given is number " + n + " is not prime ");
+
+        else 
+            System.out.println("The given is number " + n + " is prime");
+    }
+}
