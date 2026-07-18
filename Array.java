9e40248b0eb2c34fd1535410442c6fa207824e3e
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-19 00:37:31 +0530

    4.Array

--- Array.java
+++ Array.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Array {
-    
+
     public static void main(String[]args)
     {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int [] arr = new int[n];
-        for (int i = 0; i < n; i++) 
-        arr[i] = sc.nextInt();
-        int max = arr[0];
+        for (int i = 0; i < n; i++)
+            arr [i] = sc.nextInt();
+        int max = arr [0];
         for (int x : arr)
-        if (x > max)
-        max = x;
+            if (x > max)
+            max = x;
         System.out.println(max);
-
     }
 }
