e389ffa1a7182c7c60da62cc72af444ff18df30c
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-19 00:02:03 +0530

    2.Reverse

--- Reverse.java
+++ Reverse.java
@@ -4,13 +4,15 @@ public class Reverse
 {
     public static void main(String[]args)
     {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int rev = 0;
         n = Math.abs(n);
         while(n > 0)
         {
-            rev = rev * 10 + n % 10;
+
+            rev = rev * 10 + n  % 10;
             n = n / 10;
         }
         System.out.println(rev);
