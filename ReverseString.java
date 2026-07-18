41d640052b17479fedb2c9eb383415e9c0960005
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-19 00:52:03 +0530

    6. Reverse a string

--- ReverseString.java
+++ ReverseString.java
@@ -5,6 +5,7 @@ public class ReverseString {
     public static void main(String[]args)
     {
 
+        
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         System.out.println(new StringBuilder(s).reverse().toString());
