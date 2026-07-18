85c9da83b10bea6534f2d544aeb46f7fc768d058
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-19 00:12:02 +0530

    3.Prime

--- TPrime.java
+++ TPrime.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class TPrime {
 
-    public static void main(String[]args)
+    public static void main(String[]args) 
     {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Boolean isPrime = n > 1;
-        for (int i = 2; i * i <= n; i ++ )
+        for (int i = 2; i * i <= n; i++)
         {
-            if(n % i == 0)
+            if (n % i == 0)
+            {
                 isPrime = false;
                 break;
+            }
         }
         System.out.println(isPrime ? "Yes" : "No");
     }
